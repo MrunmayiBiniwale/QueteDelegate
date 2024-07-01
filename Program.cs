9fc62f99185aaf5004cee4fcf0fc4ab3f2fd2827
Mrunmayi Biniwale <mrunmayibiniwale99@gmail.com>
2024-07-01 11:54:22 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace QueteDelegate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ShowMoney showMoney = moneyInEuros;
+            Console.WriteLine(showMoney(45.54));
+
+            showMoney = moneyInDollars;
+            Console.WriteLine(showMoney(45.54));
+        }
+
+        public delegate string ShowMoney(double amount);
+
+        public static string moneyInEuros(double amount)
+        {
+            string resString = "€" + amount;
+            return resString;
+        }
+
+        public static string moneyInDollars(double amount)
+        {
+            string resString = "$" + amount;
+            return resString;
+        }
+
+    }
+}
